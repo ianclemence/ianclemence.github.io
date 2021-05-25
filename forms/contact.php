dacb5aa19a787be920dfc84af99658a82da5143d
Ian Clemence <batricupid@gmail.com>
2021-05-25 14:58:24 +0300

    Initial Commit

--- forms/contact.php
+++ forms/contact.php
@@ -1,40 +1,38 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+$message_sent = false;
+if (isset($_POST['submit']) && $_POST['g-recaptcha-response'] != "") {
 
-  $receiving_email_address = 'ianclemence17@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+  $secret = '6LcDrXAaAAAAAAIiBS_x_USQwbUc-BljtmRv0Xih'; //Your SECRET KEY is here
+  $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $_POST['g-recaptcha-response']);
+  $responseData = json_decode($verifyResponse);
+  if ($responseData->success) {
+
+    if (isset($_POST['email']) && $_POST['email'] != '') {
+
+      if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+
+        $userName = $_POST['name'];
+        $userEmail = $_POST['email'];
+        $userPhone = $_POST['phone'];
+        $quantity = $_POST['quantity'];
+        $message = $_POST['message'];
+
+        $to = "info@ezeehandle.com";
+        $messageSubject = "Email from Ezee Handle Website";
+        $body = "";
+
+        $body .= "From: " . $userName . " \r\n";
+        $body .= "Email: " . $userEmail . " \r\n";
+        $body .= "Phone: " . $userPhone . " \r\n";
+        $body .= "Quantity: " . $quantity . " \r\n";
+        $body .= "Message: " . $message . " \r\n";
+
+        mail($to, $messageSubject, $body);
+        echo $success_message;
+        $message_sentt = true;
+      }
+    }
   }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+}
 ?>
