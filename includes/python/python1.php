276349659359311e657a0f9f95790f474f86078f
Ian Clemence <batricupid@gmail.com>
2021-05-25 19:38:31 +0300

    Include File

---
+++ includes/python/python1.php
@@ -0,0 +1,12 @@
+<div class="col-lg-4 col-md-6 portfolio-item filter-python">
+    <div class="portfolio-wrap">
+        <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Python 1</h4>
+            <p>Python</p>
+            <div class="portfolio-links">
+                <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
