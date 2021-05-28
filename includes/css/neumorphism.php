0a0199bf70c1e358ee71a5c982080f4c82aa8e63
Ian Clemence <batricupid@gmail.com>
2021-05-29 01:08:19 +0300

    Initial Commit

---
+++ includes/css/neumorphism.php
@@ -0,0 +1,12 @@
+<div class="col-lg-4 col-md-6 portfolio-item filter-css">
+    <div class="portfolio-wrap">
+        <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Neumorphism</h4>
+            <p>CSS</p>
+            <div class="portfolio-links">
+                <a href="neumorphism.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
