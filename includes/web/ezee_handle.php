df21e50419fb6ba6dcfc96bdc50a55932a86c06d
Ian Clemence <batricupid@gmail.com>
2021-05-30 22:22:42 +0300

    New File

---
+++ includes/web/ezee_handle.php
@@ -0,0 +1,12 @@
+<div class="col-lg-4 col-md-6 portfolio-item filter-web">
+    <div class="portfolio-wrap">
+        <img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
+        <div class="portfolio-info">
+            <h4>Ezee-Handle</h4>
+            <p>Web</p>
+            <div class="portfolio-links">
+                <a href="ezee_handle.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
