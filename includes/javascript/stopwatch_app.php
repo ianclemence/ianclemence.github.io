b18948d811cfd10210329a21a7ccf92f57afae5e
Ian Clemence <batricupid@gmail.com>
2021-05-30 22:21:42 +0300

    Initial Commit

--- includes/javascript/stopwatch_app.php
+++ includes/javascript/stopwatch_app.php
@@ -2,10 +2,10 @@
     <div class="portfolio-wrap">
         <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
         <div class="portfolio-info">
-            <h4>Javascript 2</h4>
+            <h4>Stopwatch App</h4>
             <p>JavaScript</p>
             <div class="portfolio-links">
-                <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                <a href="stopwatch_app.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
             </div>
         </div>
     </div>
