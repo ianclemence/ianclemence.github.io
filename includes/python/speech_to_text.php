a2fa3998179c22a8a59a513ee4a433c3c572e532
Ian Clemence <batricupid@gmail.com>
2021-05-30 22:21:33 +0300

    Initial Commit

--- includes/python/speech_to_text.php
+++ includes/python/speech_to_text.php
@@ -2,10 +2,10 @@
     <div class="portfolio-wrap">
         <img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
         <div class="portfolio-info">
-            <h4>Python 3</h4>
+            <h4>Speech-To-Text</h4>
             <p>Python</p>
             <div class="portfolio-links">
-                <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                <a href="speech_to_text.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
             </div>
         </div>
     </div>
