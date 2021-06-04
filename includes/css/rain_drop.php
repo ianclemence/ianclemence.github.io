ee28b647617a27f7d08910d3daa8b33263241e65
Ian Clemence <batricupid@gmail.com>
2021-06-04 11:35:02 +0300

    Initial Commit

--- includes/css/rain_drop.php
+++ includes/css/rain_drop.php
@@ -2,10 +2,10 @@
     <div class="portfolio-wrap">
         <img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
         <div class="portfolio-info">
-            <h4>Water-Drop Effect</h4>
+            <h4>Rain Drop Animation</h4>
             <p>CSS</p>
             <div class="portfolio-links">
-                <a href="drop.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
+                <a href="https://codepen.io/ianclemence/pen/bGqvMrj" data-gallery="portfolioDetailsGallery" data-glightbox="type: external" class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
             </div>
         </div>
     </div>
